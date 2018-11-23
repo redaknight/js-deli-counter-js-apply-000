4958dfed9aba5f97bc4bcb0172482e82572d23f0
AL FATAH KADER <alfatah.kader@gmail.com>
2018-11-23 09:17:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 var katzDeliLine = [ ];
 
-function takeANumber( katzDeliLine, newName) 
-{
+function takeANumber( katzDeliLine, newName) {
     katzDeliLine.push(newName);
     return `Welcome, ${newName}. You are number ${katzDeliLine.length} in line.`;
 }
 
-function nowServing( katzDeliLine ) 
-{
+function nowServing( katzDeliLine ) {
   if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!";
   } else {
@@ -17,16 +15,13 @@ function nowServing( katzDeliLine )
     }
 }
 
-function currentLine( katzDeliLine ) 
-{
+function currentLine( katzDeliLine ) {
     var lineStatus = [ ];
     if (katzDeliLine.length === 0) {
     return "The line is currently empty.";
   } else {
-      for(var i = 0; i < katzDeliLine.length; i++)
-      {
+      for(var i = 0; i < katzDeliLine.length; i++){
         lineStatus.push(`${i + 1}. ${katzDeliLine[i]}` );
-      }
-      return `The line is currently: ${lineStatus.join(", ")}`;
+      } return `The line is currently: ${lineStatus.join(", ")}`;
     }
 }
