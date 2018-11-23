ca9bb5cc35d07f6bdb965b285d6ddd78bc64386f
AL FATAH KADER <alfatah.kader@gmail.com>
2018-11-23 08:08:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,9 +17,10 @@ function nowServing( katzDeliLine )
     }
 }
 
-function currentLine( katzDeliLine ) {
+function currentLine( katzDeliLine ) 
+{
     var lineStatus = [ ];
-   if (katzDeliLine.length === 0) {
+    if (katzDeliLine.length === 0) {
     return "The line is currently empty.";
   } else {
       for(var i = 0; i < katzDeliLine.length; i++) {
