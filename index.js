048a6a56d8e368789954f5cc9f9a70eab9fb7cb4
AL FATAH KADER <alfatah.kader@gmail.com>
2018-11-23 08:04:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function currentLine( katzDeliLine )
     return "The line is currently empty.";
   } else {
       for(var i = 0; i < katzDeliLine.length; i++) {
-        line += (i + 1) + ". " + x[i] + ", "
+        katzDeliLine += (i + 1) + ". " + katzDeliLine[i] + ", "
       }
       line = line.slice(0, line.length-2)
       return "The line is currently: " + line
