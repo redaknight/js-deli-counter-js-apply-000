f4b6950b946437841400a8d9c0adbebb221c63e2
AL FATAH KADER <alfatah.kader@gmail.com>
2018-11-23 08:56:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,9 +25,10 @@ function currentLine( katzDeliLine )
   } else {
       for(var i = 0; i < katzDeliLine.length; i++)
       {
-        lineStatus.push(`${i + 1}. ${katzDeliLine[i]}` );
+      lineStatus = lineStatus.slice( 0, lineStatus.length, `${i + 1}. ${katzDeliLine[i]}` );
       }
-      lineStatus = lineStatus.slice( 0, lineStatus.length );
       return `The line is currently: ${lineStatus.join(", ")}`;
     }
 }
+
+//    lineStatus.push(`${i + 1}. ${katzDeliLine[i]}` );
