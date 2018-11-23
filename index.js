868644e39f61134ac1c4051e4211d5d3be18eb17
AL FATAH KADER <alfatah.kader@gmail.com>
2018-11-23 07:37:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,4 +18,14 @@ function nowServing( katzDeliLine ) {
   }
 }
 
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!"
+  } else {
+    var name = katzDeliLine[0];
+    katzDeliLine.splice(0, 1);
+    return `Currently serving ${katzDeliLine}.`;
+  }
+}
+An
 
