6ead076fbe3e4789cae6ab63005fe724189ca44f
AL FATAH KADER <alfatah.kader@gmail.com>
2018-11-23 07:45:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,4 +9,12 @@ function takeANumber( katzDeliLine, newName)
   
 }
 
-
+function nowServing(x) {
+  if (x.length === 0) {
+    return "There is nobody waiting to be served!"
+  } else {
+    var name = x[0];
+    x.splice(0, 1);
+    return "Currently serving " + name + ".";
+  }
+}
